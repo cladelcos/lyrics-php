08210374890c1d37b6bab61977847220dca4e0d9
--global <--global>
2017-11-19 11:26:29 -0200

    SQL in PHP remove message of error

--- index.php
+++ index.php
@@ -5,18 +5,18 @@
     <meta charset="UTF-8">
     <title>Lyrics</title>
     <link rel="stylesheet" href="css/reset.css">
-<<<<<<< HEAD
+
     <link rel="stylesheet" media="screen and  (min-width: 250px) and (max-width: 500px)" href="css/mobile.css">
     <link rel="stylesheet" media="screen and  (max-width: 650px)" href="css/mobile.css">
     <link rel="stylesheet" media="screen and (min-width: 500px) and (max-width: 1250px)" href="css/config-page.css">
     <link rel="stylesheet" media="screen and  (min-width: 1250px) and (max-width: 2500px)" href="css/config-page.css">
-=======
+
     <link rel="stylesheet" href="css/general.css">
     <link rel="stylesheet" media="screen and  (min-width: 250px) and (max-width: 500px)" href="css/mobile.css">
     <link rel="stylesheet" media="screen and  (max-width: 650px)" href="css/mobile.css">
     <link rel="stylesheet" media="screen and (min-width: 500px) and (max-width: 1250px)" href="css/computer.css">
     <link rel="stylesheet" media="screen and  (min-width: 1250px) and (max-width: 2500px)" href="css/computer.css">
->>>>>>> a1cef4ef0ca5c12a554fb36b803ed2848d866b6b
+
     <link rel="stylesheet" href="css/animation.css">
 </head>
 <body>
@@ -27,55 +27,27 @@
     <script src="angular/model/app.js"></script>
     <script src="angular/controller/english-controller.js"></script>
 
+    <?php error_reporting(0); ?>
+
     <div class="page-screen"  ng-app="english">
         <nav>
             <ul>
                 <li><a href="#" class="disable">Lyrics</a></li>
             </ul>
         </nav>
+           <!-- O header abaixo esta com o css sendo ignorado -->
+            <h1 class="bg-headers"> <?php echo "Lyrics To Traine and Learn The English Languange" ?> </h1>
+
         <div class="body-content left-align max center">
-<<<<<<< HEAD
-            <h1>Lyrics To Traine and Learn The English Languange</h1>
-            <?php echo "Website With PHP"; ?>
-            <?php include "sql/database.php" ?>
-=======
-            <h1 class="bg-headers">Lyrics To Traine and Learn The English Languange</h1>
 
-            <?php include "sql/database.php" ?>
+
             <?php echo "PHP code in execution" ?>
->>>>>>> a1cef4ef0ca5c12a554fb36b803ed2848d866b6b
 
-            <?php
-                // se o número de resultados for maior que zero, mostra os dados
-                if($total > 0) {
-                    // inicia o loop que vai mostrar todos os dados
-                    do {
-            ?>
-                        <p><?=$linha['nome']?> / <?=$linha['telefone']?></p>
-            <?php
-                    // finaliza o loop que vai mostrar os dados
-                    }while($linha = mysql_fetch_assoc($dados));
-                // fim do if 
-                }
-            ?>
-<<<<<<< HEAD
+
             <h3>Search: <input type="text"> <button>Search</button></h3>
+            <?php include "sql/database.php" ?>
 
-            <?php
-            // tira o resultado da busca da memória
-            mysql_free_result($dados);
-            ?>
-=======
-
-
-            <h1>others results</h1>
-
-            <?php
-                // tira o resultado da busca da memória
-                mysql_free_result($dados);
-            ?>
-                
->>>>>>> a1cef4ef0ca5c12a554fb36b803ed2848d866b6b
+            
         </div>
     </div>
 </body>
--- sql/database.php
+++ sql/database.php
@@ -1,39 +1,54 @@
-<?php
-<<<<<<< HEAD
-// definições de host, database, usuário e senha
-$host = "localhost";
-$db   = "devmedia";
-$user = "root";
-$pass = "";
-// conecta ao banco de dados
-$con = mysql_pconnect($host, $user, $pass) or trigger_error(mysql_error(),E_USER_ERROR); 
-// seleciona a base de dados em que vamos trabalhar
-mysql_select_db($db, $con);
-// cria a instrução SQL que vai selecionar os dados
-$query = sprintf("SELECT id, nome, telefone FROM cadastro");
-// executa a query
-$dados = mysql_query($query, $con) or die(mysql_error());
-// transforma os dados em um array
-$linha = mysql_fetch_assoc($dados);
-// calcula quantos dados retornaram
-$total = mysql_num_rows($dados);
-=======
-	// definições de host, database, usuário e senha
-	$host = "localhost";
-	$db   = "devmedia";
-	$user = "root";
-	$pass = "";
-	// conecta ao banco de dados
-	$con = mysql_pconnect($host, $user, $pass) or trigger_error(mysql_error(),E_USER_ERROR); 
-	// seleciona a base de dados em que vamos trabalhar
-	mysql_select_db($db, $con);
-	// cria a instrução SQL que vai selecionar os dados
-	$query = sprintf("SELECT identificador, nome, telefone FROM cadastro");
-	// executa a query
-	$dados = mysql_query($query, $con) or die(mysql_error());
-	// transforma os dados em um array
-	$linha = mysql_fetch_assoc($dados);
-	// calcula quantos dados retornaram
-	$total = mysql_num_rows($dados);
->>>>>>> a1cef4ef0ca5c12a554fb36b803ed2848d866b6b
-?>
+
+<html>
+	<head>
+		
+		<link rel="stylesheet" href="../css/reset.css">
+		<link rel="stylesheet" href="../css/general.css">
+	</head>
+	<body>	
+		<?php
+
+			// definições de host, database, usuário e senha
+			$host = "localhost";
+			$db   = "devmedia";
+			$user = "root";
+			$pass = "";
+			// conecta ao banco de dados
+			$con = mysql_pconnect($host, $user, $pass) or trigger_error(mysqli_error(),E_USER_ERROR); 
+			// seleciona a base de dados em que vamos trabalhar
+			mysql_select_db($db, $con);
+			// cria a instrução SQL que vai selecionar os dados
+			$query = sprintf("SELECT id, nome, telefone FROM cadastro");
+			// executa a query
+			$dados = mysql_query($query, $con) or die(mysql_error());
+			// transforma os dados em um array
+			$linha = mysql_fetch_assoc($dados);
+			// calcula quantos dados retornaram
+			$total = mysql_num_rows($dados);
+
+		?>
+		<br>
+		<h2 class="bg-headers">Info Generate to SQL in PHP:</h2>
+		<br>
+
+		<?php
+		    // se o número de resultados for maior que zero, mostra os dados
+		    if($total > 0) {
+		        // inicia o loop que vai mostrar todos os dados
+		        do {
+		?>
+		            <p>[<?= $linha['id']?>] || <?= $linha['nome']?> / <?= $linha['telefone']?></p>
+		<?php
+		        // finaliza o loop que vai mostrar os dados
+		        }while($linha = mysql_fetch_assoc($dados));
+		    // fim do if 
+		    }
+		?>
+		<h1>others results</h1>
+
+		<?php
+		    // tira o resultado da busca da memória
+		    mysql_free_result($dados);
+		?>
+	</body>
+</html>
